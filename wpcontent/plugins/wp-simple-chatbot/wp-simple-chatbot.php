f1f903d57750c8f653c33583eaf233b6552064bb
Nathan Rossow <nathanrossow@gmail.com>
2025-05-12 13:50:30 -0500

    Update wp-simple-chatbot.php

--- wpcontent/plugins/wp-simple-chatbot/wp-simple-chatbot.php
+++ wpcontent/plugins/wp-simple-chatbot/wp-simple-chatbot.php
@@ -32,3 +32,40 @@ function wpsc_chatbot_shortcode() {
 }
 add_shortcode('wpsc_chatbot', 'wpsc_chatbot_shortcode');
 
+function wpsc_chatbot_message() {
+    check_ajax_referer('wpsc_chatbot_nonce', 'nonce');
+    $message = sanitize_text_field($_POST['message']);
+    $api_key = get_option('wpsc_openai_api_key');
+
+    if (empty($api_key)) {
+        wp_send_json_error('API key not configured.');
+    }
+
+    $response = wp_remote_post('https://api.openai.com/v1/chat/completions', [
+        'headers' => [
+            'Authorization' => 'Bearer ' . $api_key,
+            'Content-Type' => 'application/json'
+        ],
+        'body' => json_encode([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => [
+                ['role' => 'user', 'content' => $message]
+            ],
+            'max_tokens' => 150
+        ]),
+        'timeout' => 30
+    ]);
+
+    if (is_wp_error($response)) {
+        wp_send_json_error($response->get_error_message());
+    }
+
+    `\(body = json_decode(wp_remote_retrieve_body(\)`response), true);
+    if (isset($body['choices'][0]['message']['content'])) {
+        wp_send_json_success($body['choices'][0]['message']['content']);
+    } else {
+        wp_send_json_error('Failed to get response from OpenAI.');
+    }
+}
+add_action('wp_ajax_wpsc_chatbot_message', 'wpsc_chatbot_message');
+add_action('wp_ajax_nopriv_wpsc_chatbot_message', 'wpsc_chatbot_message');
